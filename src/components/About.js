6a82afcbe7f21f2028ae33f558ffb719d1376cda
Mita Amara <mita.amara@emveep.com>
2024-07-16 16:08:58 +0700

    my first commit for portfolio

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>Hello! My name is Mita Amara.</p>
+    <p>I am a web developer with experience in Ruby on Rails, React, Java, and Angular.</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projects = () => (
+  <section id="projects">
+    <h2>Projects</h2>
+    <div className="project">
+      <h3>Internal Project</h3>
+      <p>Some of the internal projects I have created are company landing pages, CMS projects for processing bicycle data such as bicycle repair shops, jewelry projects for changing programming language versions, and several other projects that are more about data processing.</p>
+    </div>
+    <div className="project">
+      <h3>Nutrifood</h3>
+      <p>Several times I have handled the Nutrifood project, where the project is confidential so it cannot be displayed or included in the link. The nutrifood project that I worked on was more about processing master data for the production process. The Nutrifood project uses the Ruby on Rails programming language with a SQL Server database.</p>
+    </div>
+    <div className="project">
+      <h3>Midnight</h3>
+      <p>In this project, I contributed more to creating the API because this project is a game for Apple and Android. The feature that I created which is quite challenging is integrating apps with other apps, namely the Twitter(x) application.</p>
+      <a href="https://www.midnight.io" target="_blank" rel="noopener noreferrer">View Project</a>
+    </div>
+  </section>
+);
+
+export default Projects;
